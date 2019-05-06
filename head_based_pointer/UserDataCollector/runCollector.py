dff1aa3980c2125dae1546404519bfb4661fbab5
muratcancicek <muratcancicek0@gmail.com>
2019-05-05 23:34:11 -0700

    Exp002 with Scott

--- head_based_pointer/UserDataCollector/runCollector.py
+++ head_based_pointer/UserDataCollector/runCollector.py
@@ -2,7 +2,7 @@ from InputOutputHandler import InputOutputHandler
 from DotWalker import DotWalker
 
 def main():
-    outputFileName = 'Exp002'
+    outputFileName = 'Exp003'
     handler = InputOutputHandler(videoSource = 1, outputVideoName = outputFileName)
     size = (720, 1280, 3) # (1080, 1920, 3) # (480, 640, 3) # 
     walker = DotWalker(size = size, maxFrameCount = 2000, outputFileName = outputFileName)
