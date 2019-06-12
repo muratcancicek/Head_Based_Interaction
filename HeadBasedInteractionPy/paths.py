62c4c24b29484aec2270095cf204fe2886a97c38
muratcancicek <muratcancicek0@gmail.com>
2019-06-11 23:49:30 -0700

    Adding InputOutputFolders with face and landmark model files

--- HeadBasedInteractionPy/paths.py
+++ HeadBasedInteractionPy/paths.py
@@ -2,18 +2,26 @@
 
 import os
 
-#if 'COMPUTERNAME' in os.environ:
-DATASETS_Folder = 'C:/cStorage/Datasets/'
+InputOutputFolders = 'InputOutputFolders/'
+DATASETS_Folder = InputOutputFolders + 'ModelData/'
+
 DLIB_face_landmark_model_path = DATASETS_Folder + 'Dlib/shape_predictor_68_face_landmarks.dat'
+
+HaarCascade_FrontalFace_path =  DATASETS_Folder + 'CV2Nets/HaarCascade/haarcascade_frontalface_default.xml'
+
 CV2Res10SSD_frozen_Folder = DATASETS_Folder + 'CV2Nets/CV2Res10SSD/'
+CV2Res10SSD_frozen_face_model_path = CV2Res10SSD_frozen_Folder + 'face68_model.txt'
+
 TF_Models_Folder = DATASETS_Folder + 'TF_Models/'
 TFMobileNetSSDFaceDetector_tf_model_path = TF_Models_Folder + 'TFMobileNetSSDFaceDetector/frozen_inference_graph_face.pb'
 YinsFacialLandmarkDetector_tf_model_path = TF_Models_Folder + 'YinsCNNBasedFacialLandmarkDetector/frozen_inference_graph.pb'
-CV2Res10SSD_frozen_face_model_path = CV2Res10SSD_frozen_Folder + 'face68_model.txt'
-DemoVideos_Folder = 'D:/GoogleDrive/Studying/Graduate/PhD Research/HeadbasedPointer/DemoVideos/'
+
+DemoVideos_Folder = InputOutputFolders + 'DemoVideos/'
 InputEstimatorsDemo_Folder = DemoVideos_Folder + 'InputEstimatorDemos/'
-HaarCascade_FrontalFace_path =  DATASETS_Folder + 'CV2Nets/HaarCascade/haarcascade_frontalface_default.xml'
-OfflineProjectFolder = 'C:/Users/MSI2/Documents/Offline_Projects/HeadBasedPointer/'
-DemoVideos_Offline_Folder = OfflineProjectFolder + 'DemoVideos/'
-Experiments_Folder = OfflineProjectFolder + 'Experiments/'
+
+if 'COMPUTERNAME' in os.environ:
+    if os.environ['COMPUTERNAME'] == "MSI2":
+        OfflineProjectFolder = 'C:/Users/MSI2/Documents/Offline_Projects/HeadBasedPointer/'
+        DemoVideos_Offline_Folder = OfflineProjectFolder + 'DemoVideos/'
+        Experiments_Folder = OfflineProjectFolder + 'Experiments/'
 
